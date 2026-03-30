95c038112ef30683aed006bcf179cddd620b809a
Kush Makkapati <kush@marble.aero>
2026-03-30 09:40:34 +0100

    Bump ver

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.6.3"
+#define THISFIRMWARE "MarblePlane V4.6.3.1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,6,3,FIRMWARE_VERSION_TYPE_OFFICIAL
